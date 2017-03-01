265a7bdae097ec470005a8d33222ab1618d8204d
杨星 <staryang@shunshunliuxue.com>
2017-03-01 22:53:52 +0800

    fix themes

--- view.php
+++ view.php
@@ -2,16 +2,13 @@
 <html>
 <head lang="en">
 <meta charset="UTF-8">
-<title>快切助手 - 超好用的网页制作&amp;专题制作助手 - 2015前端必备工具 - 勤道快切助手</title>
+<title>快切助手用例说明 - 超好用的网页制作&amp;专题制作助手 - 前端必备工具 - 快切网页制作助手</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
 <meta name="apple-itunes-app" content="app-id=821470312">
 <meta name="renderer" content="webkit">
 <meta name="format-detection" content="telephone=no">
-<meta name="searchtitle" content="快切助手,切图助手,自动切图,切片工具,自动切片工具,网页制作,网页制作助手,专题制作,专题制作助手,勤道快切助手">
-<meta name="author" content="勤道,http://kuaiqie.qdsay.com,http://www.qdsay.com">
-<meta name="copyright" content="本页版权归勤道QDsay团队协作所有.All Rights Reserved">
 <meta name="title" content="快切助手,切图助手,自动切图,切片工具,自动切片工具,网页制作,网页制作助手,专题制作,专题制作助手,勤道快切助手">
 <meta name="description" content="史上最好用的在线切图工具,自动切图并快速导出html，css样式和切片，支持响应式布局，一站打通电脑，平板，手机。"/>
 <meta name="keywords" content="快切助手,切图助手,自动切图,切片工具,自动切片工具,网页制作,网页制作助手,专题制作,专题制作助手,勤道快切助手"/>
@@ -28,7 +25,7 @@
 <header>
 <div id="headbar">
   <div id="logo">
-    <a href="http://kuaiqie.qdsay.com"><img alt="kuaiqie.qdsay.com" src="resources/images/logo.png"></a>
+    <a title="勤道快切助手" href="http://kuaiqie.qdsay.com"><img alt="kuaiqie.qdsay.com" src="resources/images/logo.png"></a>
   </div>
   <ul id="navbar">
     <li class="on"><a id="preview" class="view" title="用例预览" href="example/<?php echo $exp;?>/index.html" target="viewFrame">用例预览</a></li>
